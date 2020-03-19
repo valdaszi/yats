2a074ebe99094d37e329a4f260dc8eeb29ab83e1
Valdas <valdas@e-servisas.lt>
2020-03-19 11:51:15 +0200

    @angular/material migration - migration-v9 Updates Angular Material to v9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
-import { MatNativeDateModule } from '@angular/material'
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { AppComponent } from './app.component'
 import { CoreModule } from '@app/core/core.module'
